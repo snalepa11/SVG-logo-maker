960c0c0964e1867c96cea900fd99e9871deb2217
Snalepa <snalepa1998@gmail.com>
2024-02-25 20:36:58 -0600

    Started installs, created file structure, and started on index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const inquirer = require("inquirer")
+const {Cirle, Square, Triangle} = require("./lib/shapes")
+const
+
+const questions = [
+{
+    type: "input",
+    name: "text",
+    message: "TEXT: Enter up to 3 characters"
+},
+{
+    type: "input",
+    name: "text-color",
+    message: "TEXT COLOR: Please enter the desired color of your text via keyword or hexidecimal characters"
+},
+{
+    type: "list",
+    name: "shape",
+    message: "Please choose which image shape you would like to use",
+    choices: [Circle, Square, Triangle]
+},
+{
+    type: "input",
+    name: "shape-color",
+    message: "SHPAE COLOR: Please choose the desired color of your shape via keyword or hexidecimal characters"
+}
+]
