d168ac4bab0291177f02705f0ee4ccee9b318927
Snalepa <snalepa1998@gmail.com>
2024-02-29 17:17:07 -0600

    refined index.js logic and shape logic

--- index.js
+++ index.js
@@ -1,42 +1,64 @@
 const inquirer = require("inquirer");
-const {Circle, Square, Triangle} = require("./lib/shapes");
+const { Circle, Square, Triangle } = require("./lib/shapes");
 const fs = require("fs");
-const fileName = "./examples/logo.svg";
+const path = require('path')
+const fileNamePath = "./logo.svg";
 
 //create shape object passing data
 
-function writeToFile (fileName, answers)
-
-funstion createLogo
-
 const questions = [
-{
-    type: "input",
-    name: "text",
-    message: "TEXT: Enter up to 3 characters"
-},
-{
-    type: "input",
-    name: "textColor",
-    message: "TEXT COLOR: Please enter the desired color of your text via keyword or hexidecimal characters"
-},
-{
-    type: "list",
-    name: "shape",
-    message: "Please choose which image shape you would like to use",
-    choices: ["Circle", "Square", "Triangle"]
-},
-{
-    type: "input",
-    name: "shapeColor",
-    message: "SHPAE COLOR: Please choose the desired color of your shape via keyword or hexidecimal characters"
-}
+    {
+        type: "input",
+        name: "text",
+        message: "TEXT: Enter up to 3 characters"
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "TEXT COLOR: Please enter the desired color of your text via keyword or hexidecimal characters"
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Please choose which image shape you would like to use",
+        choices: ["Circle", "Square", "Triangle"]
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "SHPAE COLOR: Please choose the desired color of your shape via keyword or hexidecimal characters"
+    }
 ]
 
-function init(){
+function writeToFile(fileNamePath, data) {
+    fs.writeFile(fileNamePath, data, (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully");
+        }
+      });
+}
+
+
+
+function init() {
     inquirer.prompt(questions).then(answers => {
-        console.log(answers)
+        let shape;
+
+        if(answers.shape === 'Circle') {
+            shape = new Circle(answers.shapeColor, answers.textColor, answers.text);
+        }
+        else if(answers.shape === 'Square') {
+            shape = new Square(answers.shapeColor, answers.textColor, answers.text);
+        }
+        else if(answers.shape === 'Triangle') {
+            shape = new Triangle(answers.shapeColor, answers.textColor, answers.text);
+        }
+
+        writeToFile(fileNamePath, shape.render())
     })
+    
 }
 
 init()
